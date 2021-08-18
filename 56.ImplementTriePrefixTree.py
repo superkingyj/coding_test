7251809dc97313c3287b7fae2298c2cb4180626c
superkingyj <superkingyj@naver.com>
2021-08-18 12:11:51 +0900

    feat: update 56.InplementTriePrefixTree

--- 56.ImplementTriePrefixTree.py
+++ 56.ImplementTriePrefixTree.py
@@ -1,30 +1,54 @@
+import collections
+# set end possible
+# dictionary function
+# __init__(char)
+
+# TrieNode 95
+class CharNode:
+    def __init__(self):
+        self._isEnd = False
+        self._charNodes = collections.defaultdict(CharNode)
+    
+    def getNext(self, char):
+        return self._charNodes[char]
+    
+    def setEnd(self):
+        self._isEnd = True
+
+    def isIn(self, char):
+        return char in self._charNodes
+    
+    def isEnd(self):
+        return self._isEnd
+
 class Trie:
     def __init__(self):
-        self.root = {'dumy':None}
+        self.root = CharNode()
 
     def insert(self, word: str) -> None:
         pointer = self.root
         for char in word :
-            if 'dumy' in pointer : 
-                pointer[char] = {}
-                del pointer['dumy']
-            elif char not in pointer or not pointer : pointer[char] = {}
-            else : pass
-            pointer = pointer[char]
-
-        pointer['\0']=None
+           pointer = pointer.getNext(char)
+        pointer.setEnd()
 
     def search(self, word: str) -> bool:
         pointer = self.root
         for char in word :
-            if 'dumy' in pointer or char not in pointer or not pointer: return False
-            else : pointer = pointer[char]
-        if '\x00' in pointer : return True
-        return False
+            if not pointer.isIn(char): return False
+            pointer = pointer.getNext(char)
+        return pointer.isEnd()
 
     def startsWith(self, prefix: str) -> bool:
         pointer = self.root
         for char in prefix:
-            if 'dumy' in pointer or char not in pointer or not pointer: return False
-            else : pointer = pointer[char]
+            if not pointer.isIn(char): return False
+            pointer = pointer.getNext(char)
         return True
+    
+trie = Trie()
+trie.insert("apple")
+print(trie.search("apple"))
+trie.search("app")
+trie.startsWith("app")
+trie.insert("app")
+trie.search("app")
