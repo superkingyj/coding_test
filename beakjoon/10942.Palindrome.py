9e245370e997d4cbaaae7df8fb9aa868780ae05f
superkingyj <superkingyj@naver.com>
2022-03-17 13:07:16 +0900

    feat: solve beakjoon 10942.Palinedrome

--- beakjoon/10942.Palindrome.py
+++ beakjoon/10942.Palindrome.py
@@ -2,13 +2,10 @@ import sys
 
 def check_palindrome(n, m, array, question_list):
     dp = [[0] * n for _ in range(n)]
-
     for i in range(n):
         dp[i][i] = 1
-
     for i in range(n-1):
-        if array[i] == [i+1]: dp[i][i+1] = 1
-    
+        if array[i] == array[i+1]: dp[i][i+1] = 1
     for i in range(2, n):
         for j in range(n-i):
             if array[j] == array[i+j] and dp[j+1][i+j-1] == 1: dp[j][i+j] = 1
@@ -44,5 +41,13 @@ if __name__ == "__main__":
 3 3 --> question_list
 5 7 --> question_list
 
+
+7
+1 2 1 3 1 2 1
+4
+1 3
+2 5
+3 3
+5 7
  v
 '''
